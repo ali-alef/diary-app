92c5c997985003f9eaaba94cb8bfe77a5e9660fd
alialef <alialefkhani77@gmail.com>
2022-08-23 01:24:11 +0430

    spelling mistake
    correcting spelling mistake in models.py (default) and run makemigrations command

--- entries/models.py
+++ entries/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Entry(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    date = models.DateField(defualt=timezone.now)
+    date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
